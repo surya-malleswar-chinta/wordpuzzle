98daa77ea1b4c22b2dc6174d11e31be1e50158fe
surya-malleswar-chinta <malleswar.chinta@gmail.com>
2024-08-25 06:32:34 +0530

    aded WordPuzzle class wirh enum Direction

---
+++ WordPuzzle.java
@@ -0,0 +1,16 @@
+public class WordPuzzle {
+    
+    enum Direction {
+        LEFT,
+        RIGHT,
+        UP,
+        DOWN,
+        RIGHT_DIAGNOAL,
+        LEFT_DIAGNOAL
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Starting");
+    }
+
+}
